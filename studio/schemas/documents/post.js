195c0a0d411aa0503f0a265262022415fbe88979
andypbrowne <andypbrowne@gmail.com>
2022-02-06 22:39:00 -0500

    add conditional case study fields to post schema and consolidate the book categories to be the same ones used in the pots as well

--- studio/schemas/documents/post.js
+++ studio/schemas/documents/post.js
@@ -55,16 +55,22 @@ export default {
     {
       name: 'categories',
       type: 'array',
-      title: 'Categories',
+      title: 'Categorie(s)',
       group: 'content',
       of: [
         {
           type: 'reference',
           to: {
-            type: 'category'
+            type: 'bookCategory'
           }
         }
       ]
+    },
+    {
+      title: 'Case Study',
+      name: 'isCaseStudy',
+      type: 'boolean',
+      group: 'main'
     },
     {
       name: 'excerpt',
@@ -74,27 +80,59 @@ export default {
       description:
         'This ends up on summary pages, on Google, when people share your post in social media.'
     },
-    // {
-    //   name: 'authors',
-    //   title: 'Authors',
-    //   type: 'array',
-    //   of: [
-    //     {
-    //       type: 'authorReference'
-    //     }
-    //   ]
-    // },
-  
+    {
+      title: 'Overview',
+      name: 'overview',
+      type: 'excerptPortableText',
+      description: 'Summary of company or product',
+      hidden: ({document}) => !document?.isCaseStudy,
+      group: 'content'
+    },
+    {
+      title: 'Problem Statement',
+      name: 'problemStatement',
+      type: 'excerptPortableText',
+      description: 'Why did it need to happen?',
+      hidden: ({document}) => !document?.isCaseStudy,
+      group: 'content'
+    },
+    {
+      title: 'Audience',
+      name: 'audience',
+      type: 'excerptPortableText',
+      description: 'Who is it for?',
+      hidden: ({document}) => !document?.isCaseStudy,
+      group: 'content'
+    },
+    {
+      title: 'Role',
+      name: 'role',
+      type: 'excerptPortableText',
+      description: 'What did you do, who else worked on it',
+      hidden: ({document}) => !document?.isCaseStudy,
+      group: 'content'
+    },
     {
       name: 'body',
       type: 'bodyPortableText',
-      title: 'Body',
+      title: 'Body or process',
       group: 'content',
       options: {
         spellCheck: true,
       },
+    },
+    {
+      title: 'Results',
+      name: 'results',
+      type: 'excerptPortableText',
+      description: 'What happened in the end, goals achieved, lessons learned',
+      hidden: ({document}) => !document?.isCaseStudy,
+      group: 'content'
     }
   ],
+  initialValue: {
+    isCaseStudy: false
+  },
   orderings: [
     {
       name: 'publishingDateAsc',
